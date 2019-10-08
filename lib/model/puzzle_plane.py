04d3cfa41f7c5876089698df92c3179c7af15404
Jakub Mroczek <jakubmroczek2@gmail.com>
2019-10-08 11:25:28 +0200

    Puzzle plane with random shuffled numbers

---
+++ lib/model/puzzle_plane.py
@@ -0,0 +1,35 @@
+import random
+
+class PuzzlePlane:
+    
+    empty_element = 0
+
+    # initializes plane with shufffeled numbers from range <1,15>
+    def __init__(self):
+        self.plane = []
+        
+        # todo: remove magic numbers
+        numbers = []
+        for n in range(0,16):
+            numbers.append(n)
+
+        random.shuffle(numbers)
+    
+        #todo: move to distinct function
+        start = 0
+        begin = 4
+
+        #getting indices of the empty element
+        self.empty_element_index = numbers.index(self.empty_element)
+        self.empty_element_index = [int(self.empty_element_index/4), int(self.empty_element_index % 4) ]
+    
+
+        while begin <= 16:
+            self.plane.append(numbers[start:begin])
+            start += 4
+            begin += 4
+        
+
+    # returns index of an empty elemenet
+    def empty_element_index(self):
+        return self.empty_element_index
