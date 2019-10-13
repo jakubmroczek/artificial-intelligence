640315ed84ecf35c96bd8e7f66840942fa7bbebd
Jakub Mroczek <jakubmroczek2@gmail.com>
2019-10-13 15:27:14 +0200

    sketch of cli

--- __main__.py
+++ __main__.py
@@ -5,22 +5,36 @@ import argparse
 
 
 def cli():
-    pass
-    # parser = ar
+    parser = argparse.ArgumentParser(description="solve fifteen puzzle riddle using various techniques")
+    parser.add_argument("-g", "--gui", action="store_true", help="runs program with the graphical interface")
+    parser.add_argument("-r", "--random", action="store_true", help="the content of the puzzle plane riddle is "
+                                                                    "auto generated")
 
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-b", "--bfs", help="", action="store_true")
+    group.add_argument("-d", "--dfs", help="")
 
-def main():
-    row, columns = 3, 3
-    riddle = generate_random_puzzle_plane(row, columns)
-    solution = generate_ordered_puzzle_plane(row, columns)
-    print(riddle.plane)
-    print(riddle.empty_element_index)
-    print(solution.plane)
-    print(solution.empty_element_index)
-    result = bfs(riddle, solution)
-    print(len(result.moves))
-    print(result.moves)
+    parser.add_argument("rows", type=int, help="the number of rows in the puzzle plane")
+    parser.add_argument("columns", type=int, help="the number of columns in the puzzle plane")
+
+    args = parser.parse_args()
+
+    if args.random:
+        riddle = generate_random_puzzle_plane(args.rows, args.columns)
+        print(riddle.plane)
+    else:
+        riddle = []
+        raise Exception("implement user provided riddle")
+
+    solution = generate_ordered_puzzle_plane(args.rows, args.columns)
+    if args.bfs:
+        result = bfs(riddle, solution)
+        print(len(result.moves))
+        print(result.moves)
+    elif args.dfs:
+        result = dfs(riddle, solution)
+        print(result)
 
 
 if __name__ == '__main__':
-    main()
+    cli()
